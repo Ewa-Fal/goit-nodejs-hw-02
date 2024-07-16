018ab76105d4a96728ec23c3bdf6528c420275f6
Ewa <efaltynska@gmail.com>
2024-07-16 09:40:45 +0200

    add

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+
+
 
 const contactsRouter = require("./routes/api/contacts");
 
@@ -14,11 +17,11 @@ app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 
-app.use((req, res) => {
+app.use((_, res, __) => {
   res.status(404).json({ message: "Not found" });
 });
 
-app.use((err, req, res, next) => {
+app.use((err, _, res, __) => {
   res.status(500).json({ message: err.message });
 });
 
