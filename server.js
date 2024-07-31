c7b028857384346fba4dfab256fe615fc4545fbb
Ewa <efaltynska@gmail.com>
2024-07-31 21:20:46 +0200

    commit

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
-
-const app = require("./app")
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("Server running. Use our API on port: ${PORT}")
-})
-
-
 const mongoose = require("mongoose");
 const app = require("./app");
 require("dotenv").config(); 
 
-const MAIN_PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const uriDb = process.env.DB_URL;
 
 if (!uriDb) {
@@ -25,29 +15,11 @@ mongoose.connect(uriDb, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Database connection successful");
     // Uruchamiamy serwer
-    app.listen(MAIN_PORT, () => {
-      console.log(`Server is running on port ${MAIN_PORT}`);
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch((err) => {
     console.error(`Server not running. Error message: ${err.message}`);
     process.exit(1);
   });
-
-
-const MAIN_PORT = process.env.PORT || 3001;
-const uriDb = process.env.DB_URL;
-
-const connection = mongoose.connect(uriDb);
-
-connection
-  .then(() => {
-    app.listen(MAIN_PORT, function () {
-      console.log("Database connection successful");
-    });
-  })
-  .catch((err) => {
-    console.log(`Server not running. Error message: ${err.message}`);
-    process.exit(1);
-  });
-
